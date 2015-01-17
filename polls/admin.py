b3c06530019c9098a80296012198ba2fc287eb68
Cameron <cameroncairns@gmail.com>
2015-01-16 17:51:21 -0800

    Finished Part 2 of Django 2.7 Tutorial as per exercise from Tango with Django

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from polls.models import Question, Choice
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline): #Use Tabular to have a table of entries, stacked to have them stacked on one another.
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']})
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
